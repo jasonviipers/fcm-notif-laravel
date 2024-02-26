4e56416df2873b86b25c0d25e16ece2d08353b3d
jasonviipers <jason@chatr.fr>
2024-02-26 15:29:42 +0100

    Add FcmChannelTest and FcmMessageTest with test cases

--- tests/FcmChannelTest.php
+++ tests/FcmChannelTest.php
@@ -1,8 +1,6 @@
 <?php
 
-namespace Viipers\FcmNotifLaravel\Tests;
-
-use Mockery as m;
+use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Notifications\Notification;
@@ -11,5 +9,24 @@ use Viipers\FcmNotifLaravel\FcmMessage;
 
 class FcmChannelTest extends TestCase
 {
-    
+    public function testSingleNotificationIsSent()
+    {
+        // Mock Guzzle client response
+        $mockClient = $this->createMock(Client::class);
+        $mockClient->method('post')->willReturn(new Response(200, [], '{"message_id": "12345"}'));
+
+        // Instantiate FcmChannel with mocked client
+        $fcmChannel = new FcmChannel($mockClient, 'fake_api_key');
+
+        // Create mock notification
+        $notification = $this->createMock(Notification::class);
+        $notification->method('toFcm')->willReturn(new FcmMessage('device_token'));
+
+        // Send notification
+        $response = $fcmChannel->send('user', $notification);
+
+        // Assertion
+        $this->assertEquals([['message_id' => '12345']], $response);
+    }
+
 }
